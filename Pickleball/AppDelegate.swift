a76918b49d419df8cbdd10405ea659d0bb677d87
Tanner Rozier <tannerrozier@tanners-imac.local>
2019-08-31 12:07:13 -0600

    Changed everything

--- Pickleball/AppDelegate.swift
+++ Pickleball/AppDelegate.swift
@@ -17,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = UINavigationController(rootViewController: TableofTourneys())
         return true
     }
 
--- Pickleball/TableofTourneys.swift
+++ Pickleball/TableofTourneys.swift
@@ -10,32 +10,12 @@ import UIKit
 import FirebaseDatabase
 var ref : DatabaseReference!
 
-class TableofTourneys: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier = "tourneytable"
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? TourneyTableViewCell  else {
-            fatalError("The dequeued cell is not an instance of ProductTableViewCell.")
-        }
-        
-        cell.tourneyname.text = "tourney1"
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        performSegue(withIdentifier: "tourneysegue", sender: self)
-    }
+class TableofTourneys: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         ref = Database.database().reference()
+        view.backgroundColor = UIColor.blue
         
     }
     
