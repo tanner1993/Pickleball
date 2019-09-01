b8c3022ef8e971fe330973f92df7de9da79ce800
Tanner Rozier <tannerrozier@tanners-imac.local>
2019-09-01 12:54:07 -0600

    Fun Fun Fun

--- Pickleball/TourneyStandings.swift
+++ Pickleball/TourneyStandings.swift
@@ -12,6 +12,7 @@ class TourneyStandings: UICollectionViewController, UICollectionViewDelegateFlow
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(view.frame.width)
         
         navigationItem.title = "Tournament 1"
         
@@ -31,7 +32,7 @@ class TourneyStandings: UICollectionViewController, UICollectionViewDelegateFlow
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
+        return CGSize(width: view.frame.width, height: 80)
     }
 
 }
@@ -42,17 +43,65 @@ class TeamCell: UICollectionViewCell {
         setupViews()
     }
     
-    let Teammate1: UILabel = {
+    let TeamDuo: UILabel = {
         let label = UILabel()
         label.backgroundColor = UIColor.purple
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.cornerRadius = 24
+        label.layer.masksToBounds = true
+        label.text = "Teammate 1 & Teammate 2"
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textAlignment = .center
+        return label
+    }()
+    let TeamRank: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = UIColor.purple
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.cornerRadius = 35
+        label.layer.masksToBounds = true
+        label.text = "1"
+        label.font = UIFont.boldSystemFont(ofSize: 50)
+        label.textAlignment = .center
+        return label
+    }()
+    let Wins: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = UIColor.green
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.cornerRadius = 10
+        label.layer.masksToBounds = true
+        label.text = "Wins: 1"
+        label.textAlignment = .center
+        return label
+    }()
+    let Losses: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = UIColor.red
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.cornerRadius = 10
+        label.layer.masksToBounds = true
+        label.text = "Losses: 1"
+        label.textAlignment = .center
         return label
     }()
     
     func setupViews() {
-        addSubview(Teammate1)
-        addConstraintsWithFormat(format: "H:|-16-[v0]-16-|", views: Teammate1)
-        addConstraintsWithFormat(format: "V:|-16-[v0]-16-|", views: Teammate1)
+        addSubview(TeamDuo)
+        addSubview(TeamRank)
+        addSubview(Wins)
+        addSubview(Losses)
+        addConstraintsWithFormat(format: "H:|-4-[v0(70)]-4-[v1]-4-|", views: TeamRank, TeamDuo)
+        addConstraintsWithFormat(format: "V:|-4-[v0]-28-|", views: TeamDuo)
+        addConstraintsWithFormat(format: "V:|-5-[v0]-5-|", views: TeamRank)
+        addConstraint(NSLayoutConstraint(item: Wins, attribute: .top, relatedBy: .equal, toItem: TeamDuo, attribute: .bottom, multiplier: 1, constant: 4))
+        addConstraint(NSLayoutConstraint(item: Wins, attribute: .right, relatedBy: .equal, toItem: TeamDuo, attribute: .centerX, multiplier: 1, constant: -10))
+        addConstraint(NSLayoutConstraint(item: Wins, attribute: .left, relatedBy: .equal, toItem: TeamDuo, attribute: .centerX, multiplier: 1, constant: -125))
+        addConstraint(NSLayoutConstraint(item: Wins, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+        addConstraint(NSLayoutConstraint(item: Losses, attribute: .top, relatedBy: .equal, toItem: TeamDuo, attribute: .bottom, multiplier: 1, constant: 4))
+        addConstraint(NSLayoutConstraint(item: Losses, attribute: .right, relatedBy: .equal, toItem: TeamDuo, attribute: .centerX, multiplier: 1, constant: 125))
+        addConstraint(NSLayoutConstraint(item: Losses, attribute: .left, relatedBy: .equal, toItem: TeamDuo, attribute: .centerX, multiplier: 1, constant: 10))
+        addConstraint(NSLayoutConstraint(item: Losses, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
     }
     
     required init?(coder aDecoder: NSCoder) {
