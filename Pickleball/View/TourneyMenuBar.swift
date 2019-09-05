51afc42df8937813abf79356eb21609d3dcfc4cf
Tanner Rozier <tannerrozier@tanners-imac.local>
2019-09-04 21:36:33 -0600

    Added Black Bar

--- Pickleball/View/TourneyMenuBar.swift
+++ Pickleball/View/TourneyMenuBar.swift
@@ -71,22 +71,34 @@ class MenuCell: BaseCell {
         return lb
     }()
     
+    let CellBar: UILabel = {
+        let cb = UILabel()
+        cb.backgroundColor = UIColor.white
+        cb.translatesAutoresizingMaskIntoConstraints = false
+        return cb
+    }()
+    
     override var isHighlighted: Bool {
         didSet {
             CellLabels.textColor = isHighlighted ? UIColor.black : UIColor.init(displayP3Red: 211/255, green: 211/255, blue: 211/255, alpha: 1)
+            CellBar.backgroundColor = isHighlighted ? UIColor.black : UIColor.white
         }
     }
     override var isSelected: Bool {
         didSet {
             CellLabels.textColor = isSelected ? UIColor.black : UIColor.init(displayP3Red: 211/255, green: 211/255, blue: 211/255, alpha: 1)
+            CellBar.backgroundColor = isSelected ? UIColor.black : UIColor.white
         }
     }
     
     override func setupViews() {
         super.setupViews()
         addSubview(CellLabels)
+        addSubview(CellBar)
         addConstraintsWithFormat(format: "H:[v0(120)]", views: CellLabels)
         addConstraintsWithFormat(format: "V:[v0(20)]", views: CellLabels)
+        addConstraintsWithFormat(format: "H:|[v0]|", views: CellBar)
+        addConstraintsWithFormat(format: "V:[v0(5)]|", views: CellBar)
         addConstraint(NSLayoutConstraint(item: CellLabels, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
         addConstraint(NSLayoutConstraint(item: CellLabels, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
         backgroundColor = UIColor.white
